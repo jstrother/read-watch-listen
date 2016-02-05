b65865a81431c0f023608dee8e2d065a44d6a3c3
Jim Strother <strotherwebdev@gmail.com>
2016-02-05 06:36:48 +0000

    js starting to work, submit button in process

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-// tastekid api key: 201615-ReadWatc-D43NAQ4H
+$(function() {
+	$('#submit').click(function(e) {
+		e.preventDefault();
+		var searchTerm = $('#search-term').val();
+		var searchType = $('#type').val();
+		var params = {
+			q: searchTerm,
+			type: searchType,
+			info: 1,
+			limit: 15,
+			k: '201615-ReadWatc-D43NAQ4H',
+			callback: 'jsonp'
+		};
+		makeAjaxRequest(
+			'//www.tastekid.com/api/similar',
+			params,
+			'jsonp',
+			'GET',
+			function(similar) {
+				console.log(similar);
+				
+			}
+		);
+	});
+});
+
 // paramaters:
 //     q: search query
 //     type: specifies type of result (book, movie, etc)
@@ -8,15 +33,15 @@
 //     callback: jsonp
 
 function makeAjaxRequest(url,params,dataType,type,done) {
-	url = (typeof(url) == 'undefined') ? '//api.github.com/search/repositories' : url;
+	url = (typeof(url) == 'undefined') ? '//www.tastekid.com/api/similar' : url;
 	params = (typeof(params) == 'undefined') ? {} : params;
 	dataType = (typeof(dataType) == 'undefined') ? 'jsonp' : dataType;
 	type = (typeof(type) == 'undefined') ? 'GET' : type;
 	done = (typeof(done) == 'undefined') ? null : done;
 	$.ajax({
-    	url:url,
-    	data:params,
-    	dataType:dataType,
-    	type:type
+    	url: url,
+    	data: params,
+    	dataType: dataType,
+    	type: type
 	}).done(done);
 }
