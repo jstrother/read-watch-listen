f03040940fcff5aa3ba5cd6d13476c38b649b713
Jim Strother <strotherwebdev@gmail.com>
2016-02-08 00:34:35 +0000

    var i is getting confused about which value it is suppossed to be

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
 $(function() {
 	$('#submit').click(function(e) {
 		e.preventDefault();
+		// The following take in what you are searching for and what type of work it is.
 		var searchTerm = $('#search-term').val();
 		var searchType = $('#type').val();
+		// Passing in the parameters of the api callback
 		var params = {
 			q: searchTerm,
 			type: searchType,
 			info: 1,
-			limit: 15,
+			limit: 25,
 			k: '201615-ReadWatc-D43NAQ4H',
 			callback: 'jsonp'
 		};
+		// now it's time to make the actual ajax request
 		makeAjaxRequest(
-			'//www.tastekid.com/api/similar',
+			'//www.tastekid.com/api/similar', //TasteKid API is case-sensitive.  .Name, not .name, and so on.
 			params,
 			'jsonp',
 			'GET',
 			function(result) {
-				console.log(result);
 				var info = result.Similar.Info;
 				var results = result.Similar.Results;
-				console.log(info);
-				console.log(results);
-				$('#display').html('<h1>' + info[0].Name + '</h1>');
+				// show some basic info about what was searched for
+				$('#item-title').text(info[0].Name);
+				$('#item-desc').text(info[0].wTeaser);
+				// time to find something new!
+				showRecommendations(results);
 			}
 		);
 	});
@@ -30,9 +34,11 @@ $(function() {
 		e.preventDefault();
 		$('#search-term').val('').focus();
 		$('#type').val('book');
+		$('#display').hide();
+		$('#item-title').text('');
+		$('#item-desc').text('');
 	});
 });
-
 function makeAjaxRequest(url,params,dataType,type,done) {
 	url = (typeof(url) == 'undefined') ? '//www.tastekid.com/api/similar' : url;
 	params = (typeof(params) == 'undefined') ? {} : params;
@@ -45,4 +51,41 @@ function makeAjaxRequest(url,params,dataType,type,done) {
     	dataType: dataType,
     	type: type
 	}).done(done);
+}
+function showRecommendations(results) {
+	console.log(results);
+	$.each(results, function(i, item) {
+		console.log(item);
+		console.log('1 i=', i);
+		var result = $('.similar').clone().removeClass('hidden');
+		result.removeClass('similar').attr('id', 'similar' + i);
+		console.log('2 i=', i);
+		$('button').removeClass('opener').attr('id', 'opener' + i);
+		console.log('3 i=', i);
+		dialogBox(i);
+		$('#show-similar').append(result);
+	});
+	$('#display').show();
+}
+function dialogBox(i) {
+	$(function() {
+		$('#dialog' + i).dialog({
+			autoOpen: false,
+			show: {
+				effect: "blind",
+				duration: 1000
+			},
+			hide: {
+				effect: "explode",
+				duration: 1000
+			}
+		});
+		$('#opener' + i).click(function() {
+			$('#dialog' + i).dialog('open');
+		});
+	});
+}
+function convertClassID (item, value, i) {
+	item.removeClass(value).attr('id', value + i);
+	return item;
 }
