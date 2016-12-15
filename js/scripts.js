6bfc65b0133ebc66dbe5b7eddaad3e37a4a0c7fb
Jim Strother <strotherwebdev@gmail.com>
2016-12-15 05:42:29 +0000

    getting NY Times api to get some info from TasteKid, but still not working quite right

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ $(function() {
 		// The following take in what you are searching for and what type of work it is.
 		var searchTerm = $('#search-term').val(),
 			// searchType = $('#type').val(),
-			params = {
+			params1 = {
 				q: searchTerm,
 				type: 'book',
 				info: 1,
-				limit: 100,
+				limit: 50,
 				k: '201615-ReadWatc-D43NAQ4H',
 				callback: 'jsonp'
 			};
@@ -20,17 +20,11 @@ $(function() {
 		// now it's time to make the actual ajax request
 		makeAjaxRequest(
 			'//www.tastekid.com/api/similar', //TasteKid API is case-sensitive.  .Name, not .name, and so on.
-			params,
+			params1,
 			'jsonp',
 			'GET',
 			function(result) {
-				console.log('result:', result);
-				var info = result.Similar.Info,
-					results = result.Similar.Results;
-				console.log('results:', results);
-				// show some basic info about what was searched for
-				$('#item-title').text(info[0].Name);
-				$('#item-desc').text(info[0].wTeaser);
+				var results = result.Similar.Results;
 				// time to find something new!
 				showRecommendations(results);
 			}
@@ -44,38 +38,45 @@ $(function() {
 function showRecommendations(results) {
 	var itemList = [];
 	$.each(results, function(i, item) {
-		var resultType = results[i].Type,
-			resultName = results[i].Name;
+		var resultType = results[i].Type;
 		if (resultType === 'book') {
-			console.log('match:', resultType, i);
-			if (!($.inArray(resultName, itemList)))
-			console.log('resultName', resultName);
 			itemList.push(results[i]);
-			console.log('itemList', itemList);
 		}
 	});
 	var simHeader = $('#show-similar-header').find('h3');
 	simHeader.text('Similar Books');
 	$.each(itemList, function(i, item) {
-		// clone the similar div
-		var newDiv = $('#template').clone().removeClass('hidden'),
-			title = newDiv.find('h2');
+		console.log('initial item:', item);
+		var params2 = {
+				'api-key': 'b5c044b52c6042149b21672f5f28447e',
+				'title': item.Name
+			},
+			newDiv = $('#template').clone().removeClass('hidden'), // clone the similar div
+			title = newDiv.find('h2'),
+			author = newDiv.find('h4'),
+			blurb = newDiv.find('p');
+			
 		// remove the id 'template' from new clone
 		newDiv.removeAttr('id');
 		// add item title Name
 		title.text(item.Name);
-		// use NY Times api to retrieve author's name
-		// and book review  (NY Times books api key: b5c044b52c6042149b21672f5f28447e)
+		// make another ajax request to the NY Times api
+		makeAjaxRequest(
+			'//api.nytimes.com/svc/books/v3/reviews.jsonp',
+			params2,
+			'jsonp',
+			'GET',
+			function(item) {
+				console.log('NY Times item:', item);
+				// retrieve author's name
+				author.text(item.results.book_author);
+				// retrieve book review
+				blurb.text(item.results.summary);
+			}
+		);
 		$('#show-similar').append(newDiv);
 	});
 	$('#display').show();
-}
-function reset() {
-	$('#search-term').val('').focus();
-	$('#item-title').text('');
-	$('#item-desc').text('');
-	$('#show-similar').html('');
-	$('#display').hide();
 }
 function makeAjaxRequest(url,params,dataType,type,done) {
 	url = (typeof(url) == 'undefined') ? '' : url;
@@ -89,4 +90,11 @@ function makeAjaxRequest(url,params,dataType,type,done) {
     	dataType: dataType,
     	type: type
 	}).done(done);
+}
+function reset() {
+	$('#search-term').val('').focus();
+	$('#item-title').text('');
+	$('#item-desc').text('');
+	$('#show-similar').html('');
+	$('#display').hide();
 }
